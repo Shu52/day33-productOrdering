7ffd924fbdea208f8c161c271c6e6cb60475016d
Shuaib Sajid <shusajid@gmail.com>
2018-07-09 15:59:24 -0500

    intial commit, file creation

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// Practice: Product Ordering
+// Your task is to simulate a product listing page like you would find on Etsy, Amazon, or Ebay. This application should implement the following features.
+
+// Display an individual product component. Each product should have a title, short description, price, quantity, and an image.
+
+// Display the application information in a navigation bar at the top of the page.
+// The name of the company - Betsy
+// Three links: Categories, Orders, and Log Out (_these links won't actually do anything, you're just practicing making the navigation bar a component)
+// Display a review list component that contains a maximum of 3 product review components beneath a product. You can use hipster ipsum for the reviews themselves.
